7815b533ff47c297b6d22fe8ef1844fa96e1c857
Anest <efremenko_ira@mail.ru>
2023-12-12 02:47:43 +0300

    Reformate code

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 public class Main {
 
-    public static void main(String[] args){
-      int ticketPrise = 17_115;
+    public static void main(String[] args) {
+        int ticketPrise = 17_115;
 
-      int miles = ticketPrise / 20;
+        int miles = ticketPrise / 20;
         System.out.println("Бонусы: " + miles);
-
-
-
     }
 }
