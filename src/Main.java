6f3f6abe2be23cb0842a5c1d856ee6a2648128d6
Anest <efremenko_ira@mail.ru>
2023-12-12 01:02:09 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args){
+      int ticketPrise = 17_115;
+
+      int miles = ticketPrise / 20;
+        System.out.println("Бонусы: " + miles);
+
+
+
+    }
+}
